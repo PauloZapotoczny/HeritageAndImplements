1954a0c0cd7fcb0e9ffba5cdeced7b9e20d86ed1
PauloZapotoczny <paulorfz1122@gmail.com>
2024-06-02 16:25:20 -0300

    CelularesDistintos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ObjetoCelular.Models;
+
+Console.WriteLine("Smartphone: NOKIA");
+Nokia nokia = new Nokia("12344321","Modelo 12G", "31238191", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone: IPHONE");
+Iphone iphone = new Iphone("56788765", "Modelo 12 Pro",  "987987987", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
